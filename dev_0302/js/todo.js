a6a421ca30337fff97836dba473ad81bf3637b6e
Antonio Kim <antonio.kim.1994@gmail.com>
2022-03-14 23:30:31 +0900

    220314 study

--- dev_0302/js/todo.js
+++ dev_0302/js/todo.js
@@ -3,6 +3,18 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+// netToDo를 localstorage에 저장하기 위한 array
+let toDos = [];
+
+const TODOS_KEY = "todos"
+
+//JSON.stringify() : array의 item을 string으로 변환
+//JSON.parse() : string을 array로 변환
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+
+}
+
 function deleteToDo(){
     // button이 활성화 됐을 때 parentElement 정보를 통해 해당라인 삭제
     // console.dir을 통해 HTML element확인 가능
@@ -32,7 +44,25 @@ function handleToDoSubmit(event) {
     const newTodo = toDoInput.value;
     // 입력창 초기화
     toDoInput.value = "";
+    toDos.push(newTodo);
+    console.log(toDos);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHello(item) {
+    console.log("This is the turn of", item);
+}
+//parsedToDos.forEach((item) => console.log("This is the turn of ", item)); 와 같다.
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    // forEach : array의 각 item마다 function을 실행시키게 해준다.
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
