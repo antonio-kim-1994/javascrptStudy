37588aba5e2b987b56cea93d0f60ffac255115d2
Antonio Kim <antonio.kim.1994@gmail.com>
2022-03-17 23:40:45 +0900

    220317 study

---
+++ assignment/0317/src/index.js
@@ -0,0 +1,34 @@
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+  ];
+  
+  const body = document.querySelector("body");
+  const button = document.querySelector("button");
+  //console.dir(body.style);
+  
+  function handleBodyClick() {
+    const firstColor = colors[Math.floor(Math.random() * colors.length)];
+    const secondColor = colors[Math.floor(Math.random() * colors.length)];
+    body.style.background = `linear-gradient(90deg, ${firstColor}, ${secondColor})`;
+    //body.style.backgroundImage
+  }
+  
+  button.addEventListener("click", handleBodyClick);
+  
